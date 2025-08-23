cbd59f4e8bb6aa971c6bfbef80588f9d1c80786b
ritesh-gitwork <riteshmca.321@gmail.com>
2025-08-23 18:25:06 +0530

    cookies

---
+++ ExpressJS/Part 3 Cookies/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "hellooo" });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
+});
