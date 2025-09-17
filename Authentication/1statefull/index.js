6dc70c19e7be21622b124a1bcafb04f1b186162b
ritesh-gitwork <riteshmca.321@gmail.com>
2025-09-17 22:10:34 +0530

    added

--- Authentication/1statefull/index.js
+++ Authentication/1statefull/index.js
@@ -1,13 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "hellooo" });
 });
 
 app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:PORT`);
+  console.log(`server is running on http://localhost:${PORT}`);
 });
